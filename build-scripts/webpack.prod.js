d9e934a1860d1dc1a9959e131ec8b3a9d772943c
Karim Ayachi <karim@ayachi.nl>
2019-05-26 09:34:50 +0200

    Clean dist before build

--- build-scripts/webpack.prod.js
+++ build-scripts/webpack.prod.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/index.ts',
@@ -29,5 +30,8 @@ module.exports = {
         //     amd: 'knockout',
         //     root: 'ko'
         // }
-    }
+    },
+    plugins: [
+        new CleanWebpackPlugin()
+    ]
 };
